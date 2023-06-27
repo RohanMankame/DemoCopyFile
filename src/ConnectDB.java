7130895817ee26965e4df6bd6cfa419c8123c027
Rohan Mankame <rym5387@psu.edu>
2023-06-26 23:16:25 -0400

    Delete ConnectDB.java

--- src/ConnectDB.java
+++
@@ -1,8 +0,0 @@
-public class ConnectDB extends Transfer{
-
-    public void SendData(int Id,String Name,String fileDateTime, String transDateTime, boolean Success)
-    {
-        System.out.println("sending Data");
-
-    }
-}
